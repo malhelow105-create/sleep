010653e1289dfe5fd17d866e53e2c383a798ee7f
Murad Adil Mohamed Adam <malhelow105@gmail.com>
2026-02-12 11:48:30 +0200

    Update SoundSeeder.php

--- database/seeders/SoundSeeder.php
+++ database/seeders/SoundSeeder.php
@@ -17,7 +17,7 @@ class SoundSeeder extends Seeder
         [
             'name' => 'Rain',
             'file_path' => 'sounds/nature/rain.mp3',
-            'image_path' => 'images/nature/rain.png',
+            'image_path' => 'truth',
             'category' => 'Nature',
             'duration' => 3600
         ],
